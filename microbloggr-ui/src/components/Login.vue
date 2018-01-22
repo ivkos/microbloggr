9b75a3fb0c03eb2bc7c70404d26ab1a16cee1194
Ivaylo Stoyanov <me@ivkos.com>
2018-01-22 10:37:52 +0200

    UI: Add initial Login and Sign Up forms

---
+++ microbloggr-ui/src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <sui-grid class="middle aligned center aligned">
+    <div class="column">
+      <sui-form class="large">
+        <sui-segment piled>
+          <sui-form-field>
+            <sui-input name="identity" placeholder="Email address or username" icon="user" icon-position="left"/>
+          </sui-form-field>
+
+          <sui-form-field>
+            <sui-input type="password" name="password" placeholder="Password" icon="lock" icon-position="left"/>
+          </sui-form-field>
+
+          <sui-button fluid large primary>Login</sui-button>
+        </sui-segment>
+
+      </sui-form>
+
+      <sui-message class="tiny bottom attached">
+        Don't have an account?
+        <router-link to="signup">Sign Up</router-link>
+      </sui-message>
+    </div>
+  </sui-grid>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped>
+  body {
+    background-color: #DADADA;
+  }
+
+  .column {
+    max-width: 450px;
+  }
+</style>
---
+++ microbloggr-ui/src/components/SignUp.vue
@@ -0,0 +1,48 @@
+<template>
+  <sui-grid class="middle aligned center aligned">
+    <div class="column">
+      <sui-form class="large">
+        <sui-segment piled>
+          <sui-form-field>
+            <sui-input name="name" placeholder="Your name" icon="id card outline" icon-position="left"/>
+          </sui-form-field>
+
+          <sui-form-field>
+            <sui-input name="vanity" placeholder="Desired username" icon="at" icon-position="left"/>
+          </sui-form-field>
+
+          <sui-form-field>
+            <sui-input name="email" placeholder="Email" icon="mail outline" icon-position="left"/>
+          </sui-form-field>
+
+          <sui-form-field>
+            <sui-input type="password" name="password" placeholder="Password" icon="lock" icon-position="left"/>
+          </sui-form-field>
+
+          <sui-button fluid large primary>Sign Up</sui-button>
+        </sui-segment>
+      </sui-form>
+
+      <sui-message class="tiny bottom attached">
+        Already have an account?
+        <router-link to="login">Login</router-link>
+      </sui-message>
+    </div>
+  </sui-grid>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped>
+  body {
+    background-color: #DADADA;
+  }
+
+  .column {
+    max-width: 450px;
+  }
+</style>
