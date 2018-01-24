3f758a3a42b832a3232b7518a533f8d6d4b237bb
Ivaylo Stoyanov <me@ivkos.com>
2018-01-24 13:58:40 +0200

    UI: Make login work

--- microbloggr-ui/src/components/Login.vue
+++ microbloggr-ui/src/components/Login.vue
@@ -1,32 +1,92 @@
 <template>
-  <sui-grid class="middle aligned center aligned">
+  <div class="ui grid middle aligned center aligned">
     <div class="column">
-      <sui-form class="large">
-        <sui-segment piled>
-          <sui-form-field>
-            <sui-input name="identity" placeholder="Email address or username" icon="user" icon-position="left"/>
-          </sui-form-field>
+      <form class="ui form large" @submit.prevent="doLogin">
+        <div class="ui piled segment">
+          <div class="field">
+            <div class="ui left icon input">
+              <input name="identity" v-model="identity" placeholder="Email address or username"/>
+              <i class="user icon"></i>
+            </div>
+          </div>
 
-          <sui-form-field>
-            <sui-input type="password" name="password" placeholder="Password" icon="lock" icon-position="left"/>
-          </sui-form-field>
+          <div class="field">
+            <div class="ui left icon input">
+              <input type="password" v-model="password" name="password" placeholder="Password"/>
+              <i class="lock icon"></i>
+            </div>
+          </div>
 
-          <sui-button fluid large primary>Login</sui-button>
-        </sui-segment>
+          <button type="submit" class="ui button fluid large primary button">Login</button>
 
-      </sui-form>
+          <div class="ui message large negative" :hidden="errorHidden">
+            <i class="large warning sign icon"></i>
+            {{ errorMessage }}
+          </div>
+        </div>
+      </form>
 
-      <sui-message class="tiny bottom attached">
+      <div class="ui message tiny bottom attached">
         Don't have an account?
         <router-link to="signup">Sign Up</router-link>
-      </sui-message>
+      </div>
+
+      <div class="ui inverted dimmer" v-bind:class="{ active: isActive }">
+        <div class="ui loader"></div>
+      </div>
     </div>
-  </sui-grid>
+  </div>
 </template>
 
 <script>
+  import AppState from "../support/AppState";
+  import { HTTP } from "../support/http-common";
+
   export default {
-    name: 'Login'
+    name: 'Login',
+
+    data() {
+      return {
+        identity: undefined,
+        password: undefined,
+
+        isActive: false,
+        errorHidden: true,
+        errorMessage: "Something went wrong"
+      }
+    },
+
+    methods: {
+      doLogin() {
+        this.errorHidden = true;
+        this.isActive = true;
+
+        return HTTP.post("/login", {
+          identity: this.identity,
+          password: this.password
+        }).then(res => res.data)
+          .then(session => {
+            AppState.sessionId = session.sessionId;
+            AppState.isAdmin = session.admin;
+
+            return HTTP.get("/users/me")
+              .then(res => res.data)
+              .then(user => {
+                AppState.userId = user.id;
+              })
+          })
+          .then(() => {
+            this.$router.replace("/");
+          })
+          .catch(err => {
+            this.errorHidden = false;
+            this.errorMessage = err.response.data.message || "Something went wrong";
+          })
+          .finally(() => {
+            this.isActive = false;
+          })
+      }
+    }
   }
 </script>
 
