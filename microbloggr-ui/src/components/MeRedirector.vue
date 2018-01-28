3723c8add3df0b0869a91864702eee1d59365b89
Ivaylo Stoyanov <me@ivkos.com>
2018-01-29 00:48:04 +0200

    UI: MeRedirector: Use replace instead of push

--- microbloggr-ui/src/components/MeRedirector.vue
+++ microbloggr-ui/src/components/MeRedirector.vue
@@ -7,7 +7,7 @@
     name: 'MeRedirector',
     async created() {
       const res = await HTTP.get("/users/me");
-      this.$router.push(`/${res.data.vanity}`);
+      this.$router.replace(`/${res.data.vanity}`);
     }
   }
 </script>
